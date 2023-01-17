7f6eeda9c47ce24464622767003fa9f537ec5c06
yy865486385 <40563596+yy865486385@users.noreply.github.com>
2023-01-17 16:28:44 +0800

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,55 @@
-import { Table, Popconfirm, Button } from 'antd';
+import { Button } from 'antd';
+import React, { useState } from 'react';
 
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to Next.js!</h1>
-      <Button type="link" href="./about/1">
-        go to about
-      </Button>
-      <Button type="primary" danger onClick={click}>
-        primary btn
-      </Button>
-    </div>
-  );
+// export default function HomePage() {
+//   const [btnText, setbtnText] = useState('btnText');
 
-  function click(e) {
-    alert('click');
+//   function handleClick() {
+//     alert(1);
+//     setbtnText('new btnText');
+//   }
+
+//   return (
+//     <div>
+//       <h1>Welcome to Next.js!</h1>
+//       <h2>{btnText}</h2>
+//       <Button type="link" href="./about/1">
+//         go to about
+//       </Button>
+//       <Button type="primary" onClick={() => handleClick()}>
+//         {btnText}
+//       </Button>
+//     </div>
+//   );
+// }
+
+export default class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      btnText: 'btnText',
+    };
+  }
+
+  handleClick() {
+    alert(1);
+    this.setState({
+      btnText: 'newText',
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Next.js!</h1>
+        <h2>{this.state.btnText}</h2>
+        <Button type="link" href="./about/1">
+          go to about
+        </Button>
+        <Button type="primary" onClick={() => this.handleClick()}>
+          {this.state.btnText}
+        </Button>
+      </div>
+    );
   }
 }
